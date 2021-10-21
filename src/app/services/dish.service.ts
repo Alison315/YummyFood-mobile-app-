aa697b19385f6f31cab21c2ee972b2c4a4be8471
Alison Nan <alisonnan315@gmail.com>
2021-10-21 12:48:18 +1000

    dishdetail seperate page done

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -6,10 +6,13 @@ import { Dish } from '../shared/dish'
   providedIn: 'root'
 })
 export class DishService {
-
+  
   constructor() { }
   getDishes(){
     return DISHES;
+  }
+  getDish(id:string): Dish {
+    return DISHES.filter((dish) => dish.id=id)[0];
   }
   getFeaturedDish(): Dish {
     return DISHES.filter((dish) => dish.featured)[0];
